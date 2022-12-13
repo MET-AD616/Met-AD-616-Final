09bfe15ac4a13f65abf59e2ab8e68219a9ef8d9f
jcarbaugh7 <carbaugh@bu.edu>
2022-12-13 14:52:01 -0500

    Added alternate simulation

--- Jack's Code.R
+++ Jack's Code.R
@@ -2,122 +2,111 @@
 #Boyu's simulation code with minor change
 library(tidyverse)
 
-## import data
-Peak <- read.csv("PeakData.csv", header = T)
-NonPeak <- read.csv("NonPeakData.csv", header = T)
-Peak_1 <- Peak[-1,-1]
-NonPeak_1 <- NonPeak[-1,-1]
-Peak_RG <- Peak_1 
-Peak_RG[lower.tri(Peak_1)] <- 0 
 
 
 
-### Simulate Matrix
-## Peak_RG SIM
-## lower.tri
-Peak_RG_SIM <- array(dim=c(21,21,200))
-for (i in 1:21){
-  for(j in 1:21){
-    Peak_RG_SIM [j,i,] <- rnorm(n=200, mean=Peak_RG[j,i,], sd=Peak_RG[j,i,]/5) %>% round()
+###############################################################
+#Simulation accounting for bus capacity
+
+#Import and pivot data
+Peak <- read.csv("PeakData.csv", header = T)[-1,]
+NonPeak <- read.csv("NonPeakData.csv", header = T)[-1,]
+Peak_p <- pivot_longer(Peak, cols = c(-Bus.Stop.Name),
+                       names_to = "Destination",
+                       values_to = "Mean_Passengers") %>%
+  mutate(num_origin = rep(1:21,each=21),
+         num_dest = rep(1:21,times=21))
+NonPeak_p <- pivot_longer(NonPeak, cols = c(-Bus.Stop.Name),
+                       names_to = "Destination",
+                       values_to = "Mean_Passengers") %>%
+  mutate(num_origin = rep(1:21,each=21),
+         num_dest = rep(1:21,times=21))
+
+#North route for Peak Hours
+Peak_n <- filter(Peak_p, num_origin < num_dest)
+#South route for Peak Hours
+Peak_s <- filter(Peak_p, num_origin > num_dest) %>%
+  mutate(num_origin = -1*(num_origin-22), num_dest = -1*(num_dest-22))
+#North route for NonPeak Hours
+NonPeak_n <- filter(NonPeak_p, num_origin < num_dest)
+#South route for NonPeak Hours
+NonPeak_s <- filter(NonPeak_p, num_origin > num_dest) %>%
+  mutate(num_origin = -1*(num_origin-22), num_dest = -1*(num_dest-22))
+
+
+#Function that determines who gets left off the bus if the limit is met
+#*Only used within one_run function
+limit_adjust <- function(d, num_change,stop){
+  vals <- filter(d,num_origin == stop)$Mean_Passengers
+  dest_start <- stop + 1
+  vals_long <- rep(dest_start:21,times=vals)
+  ran_choice <- sample(vals_long, size=num_change, replace = F)
+  
+  for (num in ran_choice) {
+    d$Mean_Passengers[d$num_origin==stop & d$num_dest == num] <- d$Mean_Passengers[d$num_origin==stop & d$num_dest == num]-1
   }
+  return(d)
+  
 }
 
-
-Peak_RG_SIM2 <- array(dim=c(21,3,200))
-x1<-array(dim=c(1,21,200))
-y1<-array(dim=c(1,21,200))
-z1<-array(dim=c(1,21,200))
-for (i in 1:200){
-  x1[,,i]<-cumsum(rowSums(Peak_RG_SIM[,,i],na.rm = TRUE))
-  y1[,,i]<-cumsum(colSums(Peak_RG_SIM[,,i],na.rm = TRUE))  
-  z1 <- x1-y1
-  Peak_RG_SIM2[,,i]<- as.matrix(cbind(x1[,,i],y1[,,i],z1[,,i]))
-}
-z1
-Peak_RG_SIM2
-
-
-## upper.tri
-Peak_RG2 <- Peak_1 
-Peak_RG2[upper.tri(Peak_1)] <- 0 
-#Reverses the column and row order of the matrix
-Peak_RG2 <- Peak_RG2[, rev(seq_len(ncol(Peak_RG2)))]
-Peak_RG2 <- Peak_RG2[rev(seq_len(nrow(Peak_RG2))), ]
-
-upPeak_RG_SIM <- array(dim=c(21,21,200))
-for (i in 1:21){
-  for(j in 1:21){
-    upPeak_RG_SIM [j,i,] <- rnorm(n=200, mean=Peak_RG2[j,i,], sd=Peak_RG2[j,i,]/5) %>% round()
+#Runs one simulation for the bus routes
+one_run <- function(d, limit, buses){
+  d$Mean_Passengers <- rnorm(n=length(d$Mean_Passengers), mean = d$Mean_Passengers, sd = d$Mean_Passengers/5) %>% round()
+  
+  d <- mutate(d, Mean_Passengers = Mean_Passengers%/%buses + ifelse(runif(1,0,1) > Mean_Passengers%%buses/buses, 0,1))
+  
+  result <- data.frame(passengers = rep(0,times=21),total=rep(0,times=21))
+  
+  for (stop in 1:21){
+    get_on <- sum(filter(d,num_origin == stop)$Mean_Passengers)
+    get_off <- sum(filter(d,num_dest == stop)$Mean_Passengers)
+    
+    result[stop,1] <- get_on-get_off
+    result$total <- cumsum(result$passengers)
+    
+    if (result[stop,2] > limit) {
+      num_change <- result[stop,2] - limit
+      d <- limit_adjust(d,num_change,stop)
+      
+      get_on <- sum(filter(d,num_origin == stop)$Mean_Passengers)
+      get_off <- sum(filter(d,num_dest == stop)$Mean_Passengers)
+      
+      result[stop,1] <- get_on-get_off
+      result$total <- cumsum(result$passengers)
+    }
   }
+  return(result$total)
 }
 
 
-upPeak_RG_SIM2 <- array(dim=c(21,3,200))
-x2<-array(dim=c(1,21,200))
-y2<-array(dim=c(1,21,200))
-z2<-array(dim=c(1,21,200))
-for (i in 1:200){
-  x2[,,i]<-cumsum(rowSums(upPeak_RG_SIM[,,i],na.rm = TRUE))
-  y2[,,i]<-cumsum(colSums(upPeak_RG_SIM[,,i],na.rm = TRUE))  
-  z2 <- x2-y2
-  upPeak_RG_SIM2[,,i]<- as.matrix(cbind(x2[,,i],y2[,,i],z2[,,i]))
-}
-z2
-upPeak_RG_SIM2
-
-## Non-Peak
-## lower.tri
-NonPeak <- read.csv("NonPeakData.csv", header = T)
-NonPeak_1 <- NonPeak[-1,-1]
-NonPeak_RG <- NonPeak_1 
-NonPeak_RG[lower.tri(NonPeak_1)] <- 0
-
-NonPeak_RG_SIM <- array(dim=c(21,21,200))
-for (i in 1:21){
-  for(j in 1:21){
-    NonPeak_RG_SIM [j,i,] <- rnorm(n=200, mean=NonPeak_RG[j,i,], sd=NonPeak_RG[j,i,]/5) %>% round()
+#Runs a simulation for multiple trials of the bus route
+#d : dataset (either Peak_n, Peak_s, NonPeak_n, or NonPeak_s)
+#trials: Number of trials for simulation
+#limit: The maximum amount of people that can fit on the bus
+#buses: The amount of buses available
+#north: boolean, True for north route, False for south route
+run_sim <- function(d, trials, limit, buses, north){
+  sim <- one_run(d, limit,buses)
+  for (run in 2:trials) {
+    sim <- rbind(sim,one_run(d,limit,buses))
   }
-}
-
-
-NonPeak_RG_SIM2 <- array(dim=c(21,3,200))
-x3<-array(dim=c(1,21,200))
-y3<-array(dim=c(1,21,200))
-z3<-array(dim=c(1,21,200))
-for (i in 1:200){
-  x3[,,i]<-cumsum(rowSums(NonPeak_RG_SIM[,,i],na.rm = TRUE))
-  y3[,,i]<-cumsum(colSums(NonPeak_RG_SIM[,,i],na.rm = TRUE))  
-  z3 <- x3-y3
-  NonPeak_RG_SIM2[,,i]<- as.matrix(cbind(x3[,,i],y3[,,i],z3[,,i]))
-}
-z3
-NonPeak_RG_SIM2
-
-
-## upper.tri
-upNonPeak_RG <- NonPeak_1 
-upNonPeak_RG[upper.tri(NonPeak_1)] <- 0
-#Reverses the column and row order of the matrix
-upNonPeak_RG <- upNonPeak_RG[, rev(seq_len(ncol(upNonPeak_RG)))]
-upNonPeak_RG <- upNonPeak_RG[rev(seq_len(nrow(upNonPeak_RG))), ]
-
-upNonPeak_RG_SIM <- array(dim=c(21,21,200))
-for (i in 1:21){
-  for(j in 1:21){
-    upNonPeak_RG_SIM [j,i,] <- rnorm(n=200, mean=upNonPeak_RG[j,i,], sd=upNonPeak_RG[j,i,]/5) %>% round()
+  sim <- data.frame(sim)
+  if (north) {
+    colnames(sim) <- make.names(Peak$Bus.Stop.Name)
   }
+  else{
+    colnames(sim) <- make.names(rev(Peak$Bus.Stop.Name))
+  }
+    rownames(sim) <- 1:nrow(sim)
+  return(sim)
+  
 }
 
+#run_sim only function that should be run
+#Warning: Simulation slow... takes about 30 seconds for 200 trials
+y <- run_sim(d = Peak_n,
+             trials = 200,
+             limit = 200,
+             buses = 20,
+             north = T)
 
-upNonPeak_RG_SIM2 <- array(dim=c(21,3,200))
-x4<-array(dim=c(1,21,200))
-y4<-array(dim=c(1,21,200))
-z4<-array(dim=c(1,21,200))
-for (i in 1:200){
-  x4[,,i]<-cumsum(rowSums(upNonPeak_RG_SIM[,,i],na.rm = TRUE))
-  y4[,,i]<-cumsum(colSums(upNonPeak_RG_SIM[,,i],na.rm = TRUE))  
-  z4 <- y4-x4
-  upNonPeak_RG_SIM2[,,i]<- as.matrix(cbind(x4[,,i],y4[,,i],z4[,,i]))
-}
-z4
-upNonPeak_RG_SIM2
